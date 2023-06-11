04a4c62175cf0b1f14fcbada65dc6eb7d64d462d
Devansh Suri <33149887+devansh19@users.noreply.github.com>
2023-06-11 16:10:09 +0530

    Update 12-rock-paper-scissors.js

--- 12-rock-paper-scissors.js
+++ 12-rock-paper-scissors.js
@@ -16,12 +16,18 @@ score = {
 }
 }
 */
+
+document.querySelector('.js-auto-play-button').
+  addEventListener('click', () => {
+    autoPlay();
+  })
+
 let isAutoPlaying = false;
 let intervalId;
 
 function autoPlay() {
   if(!isAutoPlaying){
-      intervalId = setInterval(function() {
+      intervalId = setInterval(() => {
         const playerMove = pickComputerMove();
         playGame(playerMove);
       }, 1000);
@@ -32,6 +38,32 @@ function autoPlay() {
     isAutoPlaying = false;
   }
 }
+
+document.querySelector('.js-rock-button')
+.addEventListener('click', () => {
+  playGame('rock');
+});
+
+document.querySelector('.js-paper-button')
+.addEventListener('click', () => {
+  playGame('paper');
+});
+document.querySelector('.js-paper-button')
+.addEventListener('click', () => {
+  playGame('scissors');
+});
+
+document.body.addEventListener('keydown', (event) =>{
+  if(event.key === 'r'){
+    playGame('rock');
+  }else if (event.key === 'p') {
+    playGame('paper');
+  }else if(event.key === 's') {
+    if(event.key === 's'){
+      playGame('scissors');
+    }
+  }
+})
 function playGame(playerMove) {
 const computerMove = pickComputerMove();
 
