e6bad55223e245163c0254571a99b44648726746
Devansh Suri <33149887+devansh19@users.noreply.github.com>
2023-06-11 16:09:10 +0530

    Update 12-todo-list.js

--- 12-todo-list.js
+++ 12-todo-list.js
@@ -10,47 +10,40 @@
    function renderTodo(){
       let todoisHTML = '';
 
-      todoList.forEach(function(todoObject, index){
+      todoList.forEach((todoObject, index) => {
    
          const { name, dueDate } = todoObject;
          const html=`
            <div>${name}</div>
            <div>${dueDate}</div>
-           <button onclick="
-              todoList.splice(${index},1);
-              renderTodo();
-            " class = "delete-todo-button">Delete</button>
+           <button class = "delete-todo-button delete-button">Delete</button>
          
           `;
          todoisHTML += html;
-      });
-      /*
-      for(let i=0;i<todoList.length;i++) {
-         const todoObject =todoList[i];
-         //const name = todoObject.name;
-         //const dueDate = todoObject.dueDate;
-         const { name, dueDate } = todoObject;
-         const html=`
-           <div>${name}</div>
-           <div>${dueDate}</div>
-           <button onclick="
-              todoList.splice(${i},1);
-              renderTodo();
-            " class = "delete-todo-button">Delete</button>
-         
-          `;
-         todoisHTML += html;
-      }
-      */
-      console.log(todoisHTML);
 
-      document.querySelector('.js-todo-list').innerHTML = todoisHTML;
+      });
+
+      document.querySelector('.js-todo-list')
+      .innerHTML = todoisHTML;
+
+      document.querySelectorAll('.delete-button')
+      .forEach((deleteButton, index) => {
+         deleteButton.addEventListener('click', () => {
+            todoList.splice(index, 1);
+            renderTodo();
+         })         
+      })
    }
       function handleTodoKeydown(event) {
          if(event.key === 'Enter') {
             addTodo();
          }
       }
+
+      document.querySelector('.add-button').
+      addEventListener('click', () => {
+           addTodo();
+      })
       function addTodo() {
          const inputElement = document.querySelector('.js-name-input');
          const name = inputElement.value;
